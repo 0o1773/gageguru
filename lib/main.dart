65cbbbfe1656803b6eee9af521d65bd43332d4b2
天野 孝汰 <amakou1019@gmail.com>
2024-09-03 00:15:18 +0900

    feat: apply material theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'util.dart';
+import 'theme.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "Gage Guru",
-    theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey)),
-    home: Home(),
-  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final brightness = View.of(context).platformDispatcher.platformBrightness;
+    TextTheme textTheme = createTextTheme(context, "M PLUS 1p", "M PLUS 2");
+    MaterialTheme theme = MaterialTheme(textTheme);
+    return MaterialApp(
+      title: 'GageGuru',
+      theme: theme.light(),
+      home: const Home(),
+    );
+  }
 }
 
 class Home extends StatelessWidget {
